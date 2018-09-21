5d4dcc3f3f964031de3396a77414a064081922ad
BigDaddyCodes <!!urDED2>
2018-09-21 05:17:51 -0400

    Work on CopyManager

---
+++ DonationProgram/src/group/project/Donor.java
@@ -0,0 +1,13 @@
+/*
+ * By using this program you agree to hold the authors harmless
+ * from any damages incurred.
+ */
+package group.project;
+
+/**
+ *
+ * @author Brian, Dondra, Jedidiah, Kevin, Timia
+ */
+public class Donor {
+    
+}
