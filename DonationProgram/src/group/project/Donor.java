68c68ed7f957215de6eaefa4406c8c42f70b83d4
BigDaddyCodes <!!urDED2>
2018-09-27 11:52:11 -0400

    Donor code updates

--- DonationProgram/src/group/project/Donor.java
+++ DonationProgram/src/group/project/Donor.java
@@ -9,5 +9,36 @@ package group.project;
  * @author Brian, Dondra, Jedidiah, Kevin, Timia
  */
 public class Donor {
+    private String name = new String();
+    private double[] amountDonated;
+    private String[] areaDonated;
+    
+    public Donor (String n, double amt, String area){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double[] getAmountDonated() {
+        return amountDonated;
+    }
+
+    public void setAmountDonated(double[] amountDonated) {
+        this.amountDonated = amountDonated;
+    }
+
+    public String[] getAreaDonated() {
+        return areaDonated;
+    }
+
+    public void setAreaDonated(String[] areaDonated) {
+        this.areaDonated = areaDonated;
+    }
     
 }
