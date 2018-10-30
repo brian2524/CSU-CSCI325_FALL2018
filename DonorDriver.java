1d99e4e8d879240a8fefcd2ad64e414ca7262bca
Big Daddy <35257030+BigDaddyCodes@users.noreply.github.com>
2018-10-30 08:54:04 -0400

    Add files via upload

---
+++ DonorDriver.java
@@ -0,0 +1,32 @@
+/*
+ * By using this program you agree to hold the authors harmless
+ * from any damages incurred.
+ */
+package group.project;
+
+/**
+ *
+ * @author Jedidiah Bird
+ */
+public class DonorDriver {
+    
+    
+    public static void main(String[] args) {
+        
+        DonorMain donorList = new DonorMain();
+        
+        donorList.addDonor(new Donor("Bob R", "Linda", "Donger", 37.00, "CASH",
+                "Tithes", false));
+        donorList.addDonor(new Donor("Bill", "Ella", "Hickok", 25, "1788",
+                "Tithes", false));
+        donorList.addDonor(new Donor("Biff G", "Sally E", "Tannen", 17, "CASH",
+                "Tithes", true));
+        donorList.addDonor(new Donor("Bart", "Tannen", 17, "1234", "Tithes", false));
+        
+        donorList.getDonors();
+        
+        donorList.getDonor("alice");
+        
+    }
+    
+}
