6b96d7f002d31b977a059931978473e9a2ef2413
reneerich70 <dondra.drenee@gmail.com>
2018-11-27 06:06:48 -0500

    changes to scene, stage, removed tableview, added arraylist,two column grid

--- DonationProgram/src/group/project/NewDonorReport.java
+++ DonationProgram/src/group/project/NewDonorReport.java
@@ -9,13 +9,13 @@ import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.control.cell.PropertyValueFactory;
+
 import java.util.ArrayList;
+import javafx.scene.layout.GridPane;
 
 
 
@@ -24,41 +24,43 @@ import java.util.ArrayList;
  * PURPOSE: This class will print a report of all new donors entered
  * @author Dondra Crocker Richards
  */
-public class NewDonorReport  extends AddNewDonor{
-    private final TableView<Donor>  newDonorTable = new TableView<>();    
+public class NewDonorReport  {
+    private Stage newDonRptStage = new Stage();
+    //constructor
+    public NewDonorReport(){     
+            
+            }
+    private void displayNewDonRpt (ArrayList<Donor>newDon){
+        //iterate the new donor to display the names, and notes of the new donor
+        //per Kevin's test code in Reports
+        
+        //create a grid pane
+        GridPane newDonGrid = new GridPane();
+        //create an hbox node
+        HBox newDonHbox = new HBox(400);
+        newDonHbox.setPadding(new Insets(15,15,15,15));
+        newDonHbox.getChildren().add(new Label("Last Name: "));
+        newDonHbox.getChildren().add(new Label("First Name1: "));
+        newDonHbox.getChildren().add(new Label("First Name2: "));  
+        newDonHbox.getChildren().add(new Label("Notes: "));
+        
+        
+        for (int index = 0; index < newDon.size(); index++){
+            HBox.setMargin(newDonHbox, new Insets(0,0,0,15));
+            newDon.get(index).getName();
+            newDon.get(index).getNotes();
+            
+        }
+    Scene newDonRpt = new Scene(newDonGrid);
+
+        newDonRptStage.setScene(newDonRpt);
+        newDonRptStage.setTitle("New Donors Rpeort");
+        newDonRptStage.setWidth(800);
+        newDonRptStage.setHeight(400);
+        newDonRptStage.show();
+}
+            
   
-    Stage newDonorRptStage = new Stage();
-    Scene donRptScene = new Scene(new Group());    
-    
-    public NewDonorReport(ArrayList<Donor>newDon)
-    {
-        newDonorRptStage.setTitle("New Donors");
-        newDonorRptStage.setWidth(300);
-        newDonorRptStage.setHeight(500);
-        final Label nDnrRptLabel = new Label("New Donor Report");
-        nDnrRptLabel.setFont(new Font("Arial", 20));       
-        
-       
-        
-        TableColumn lstName = new TableColumn("Last(Family) Name");
-        lstName.setMinWidth(100);
-        lstName.setCellValueFactory(new PropertyValueFactory<>(super.getLastName()));
-        TableColumn frstName1 = new TableColumn("First Name1");
-        frstName1.setMinWidth(100);
-         frstName1.setCellValueFactory(new PropertyValueFactory<>(super.getFirstName1()));
-        TableColumn frstName2 = new TableColumn("First Name2");
-         frstName2.setCellValueFactory(new PropertyValueFactory<>(super.getFirstName2()));
-        TableColumn nDnrNote = new TableColumn("Notes: ");
-         nDnrNote.setCellValueFactory(new PropertyValueFactory<>(super.getNotes()));
-        newDonorTable.getColumns().addAll(lstName, frstName1, frstName2, nDnrNote);
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10,0,0,10));
-        vbox.getChildren().addAll(nDnrRptLabel, newDonorTable);
-        ((Group) donRptScene.getRoot()).getChildren().addAll(vbox);
-        newDonorRptStage.setScene(donRptScene);
-        newDonorRptStage.show();        
-    }  
-    
+   
     
 }
