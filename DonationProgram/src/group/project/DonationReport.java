ad4bc711d9e48b22565e09a4110b98a6cf396391
sonicthe <kglancaster@csustudent.edu>
2018-11-28 23:05:00 -0500

    Format amounts in DonationReport

--- DonationProgram/src/group/project/DonationReport.java
+++ DonationProgram/src/group/project/DonationReport.java
@@ -1,19 +1,16 @@
 package group.project;
 
 import java.util.ArrayList;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -42,36 +39,58 @@ public class DonationReport {
         Label paymentType = new Label("Cash Or Check Num");
         
         GridPane gridPane = new GridPane();
-        
+        Button exit = new Button("Exit Report View");
+            exit.setFont(Font.font("Times New Roman", 15.75));
+            exit.setAlignment(Pos.CENTER);
+            exit.setMinHeight(30);
+            exit.setMaxHeight(30);
+            exit.setMinWidth(300);
+            exit.setMaxWidth(300);
         gridPane.addColumn(0, name);
         gridPane.addColumn(1, offType);
         gridPane.addColumn(2, amount);
         gridPane.addColumn(3, paymentType);
         gridPane.setHgap(20);
+        gridPane.setVgap(5);
         gridPane.setAlignment(Pos.CENTER);
+        //gridPane.setGridLinesVisible(true);
+        ColumnConstraints rightAlign = new ColumnConstraints();
+            rightAlign.setHalignment(HPos.RIGHT);
+        ColumnConstraints leftAlign = new ColumnConstraints();
+            leftAlign.setHalignment(HPos.LEFT);
+        gridPane.getColumnConstraints().add(0, leftAlign);
+        gridPane.getColumnConstraints().add(1, leftAlign);
+        gridPane.getColumnConstraints().add(2, rightAlign);
+        gridPane.getColumnConstraints().add(3, leftAlign);
         
+        VBox vBoxAll = new VBox(gridPane, exit);
+        vBoxAll.setSpacing(50);
+        vBoxAll.setAlignment(Pos.CENTER);
         
-        Scene scene = new Scene(gridPane);
+        Scene scene = new Scene(vBoxAll);
     
         
         
         for (int i =0; i < wkDon.size(); i++)
         {
             Double temp = wkDon.get(i).getDonationAmt();
+            String tStr = (String.format("$%.2f", temp));
             Label nam = new Label(wkDon.get(i).getDonorName());
             Label off = new Label(wkDon.get(i).getDonationType());
-            Label amoun = new Label(temp.toString());
+            Label amoun = new Label(tStr);
             Label pay = new Label(wkDon.get(i).getCheckCash());
             
 
             
             gridPane.addRow(i + 1, nam, off, amoun, pay);
         }
-       
+        exit.setOnAction(e -> {primaryStage.close();});       
         primaryStage.setScene(scene);
         primaryStage.setTitle("Weekly Donation Report");
         primaryStage.setMaximized(true);
         primaryStage.showAndWait();
+        
+
     }
         
 
