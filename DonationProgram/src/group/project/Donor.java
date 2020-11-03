84283ba5b5a64e8b81000f1c1640f86fd4db110f
Brian Hinkle <b2hinkle@gmail.com>
2020-11-02 21:00:47 -0500

    Update Donor.java

--- DonationProgram/src/group/project/Donor.java
+++ DonationProgram/src/group/project/Donor.java
@@ -1,6 +1,14 @@
 /*
+ * Student Name: Brian Hinkle
+ * Program Name: Donation Program
+ * Creation Date: 11/26/2018
+ * Last Modified Date: 11/2/2020
+ * CSCI Course: CSCI 325
+ * Grade Received: A
+ * Design Comments:
  * By using this program you agree to hold the authors harmless
  * from any damages incurred.
+ * @author Jedidiah Bird
  */
 package group.project;
 
@@ -8,11 +16,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-/**
- *
- * @author Jedidiah Bird
- */
 
 public class Donor {
     private String firstName;
